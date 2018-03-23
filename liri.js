6308a09b272980ed8121fe6b354962b98e749c4c
dtroxler21 <dtroxler21@gmail.com>
2018-03-23 13:23:02 -0400

    adding function for OMDB API

--- liri.js
+++ liri.js
@@ -16,9 +16,9 @@ if (process.argv[2] === "my-tweets") {
     getTweets();
 } else if (process.argv[2] === "spotify-this-song") {
     getSong();
-} // else if (process.argv[2] === "movie-this") {
-//     getMovie();
-// } else if (process.argv[2] === "do-what-it-says") {
+} else if (process.argv[2] === "movie-this") {
+    getMovie();
+} // else if (process.argv[2] === "do-what-it-says") {
 //     whatItSays();
 // };
 
@@ -34,7 +34,14 @@ function getTweets() {
 };
 
 function getSong() {
-    var songInput = "The Sign Ace of Base";
+    var songInput = "";
+    if (!process.argv[3]) {
+        songInput = "The Sign Ace of Base";
+    } else {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            songInput = songInput + " " + nodeArgs[i];
+        }
+    };
     spotify.search({
         type: 'track',
         query: songInput
@@ -42,10 +49,36 @@ function getSong() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log(data.tracks.items[0]);
         console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
         console.log("\nSong Name: " + data.tracks.items[0].name);
         console.log("\nSong Link: " + data.tracks.items[0].preview_url);
         console.log("\nAlbum Name: " + data.tracks.items[0].album.name);
+    });
+};
+
+function getMovie() {
+    var movieInput = "";
+    if (!process.argv[3]) {
+        movieInput = "Mr.+Nobody";
+    } else {
+        for (var i = 3; i < nodeArgs.length; i++) {
+            movieInput = movieInput + "+" + nodeArgs[i];
+        }
+    };
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+
+                // If the request is successful
+                if (!error && response.statusCode === 200) {
+                    console.log("Movie Name: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                    console.log("Country: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                }
     });
 };
