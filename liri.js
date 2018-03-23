ab704562f35d5e2d6f7f391139ef84e2e45fd8a0
dtroxler21 <dtroxler21@gmail.com>
2018-03-23 12:34:04 -0400

    adding functionality to twitter function

--- liri.js
+++ liri.js
@@ -10,3 +10,25 @@ var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+var nodeArgs = process.argv;
+
+if (process.argv[2] === "my-tweets") {
+    getTweets();
+ }// else if (process.argv[2] === "spotify-this-song") {
+//     getSong();
+// } else if (process.argv[2] === "movie-this") {
+//     getMovie();
+// } else if (process.argv[2] === "do-what-it-says") {
+//     whatItSays();
+// };
+
+function getTweets() {
+    var params = {
+        screen_name: 'MyNameIsLiri21'
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets.text);
+        }
+    });
+};
