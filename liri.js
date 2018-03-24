5ac354c42e3fece5a8fbe56b39dd26636e2b651f
dtroxler21 <dtroxler21@gmail.com>
2018-03-24 09:17:59 -0400

    adding comments and final functionality

--- liri.js
+++ liri.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 
+// Using npm require to grab files and packages
 var request = require("request");
 var keys = require("./keys.js");
 var fs = require("fs");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api")
 
-
+// Creating variables for the Spotify and Twitter keys from keys.js
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+// Creating a variable for the console arguments
 var nodeArgs = process.argv;
 
+// Finding out what command is being asked and running their respective function
 if (process.argv[2] === "my-tweets") {
     getTweets();
 } else if (process.argv[2] === "spotify-this-song") {
@@ -22,33 +25,45 @@ if (process.argv[2] === "my-tweets") {
     whatItSays();
 };
 
+// Function to get the tweets from alias account
 function getTweets() {
     var params = {
         screen_name: 'MyNameIsLiri21'
     };
+    // Using the keys we are getting the tweets and displaying them on the console
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        // Only displaying the tweets if there is not an error
         if (!error) {
-            console.log(tweets.text);
-        }
+            for (var i=0; i < 20; i++) {
+                console.log(tweets[i].text);
+            };
+        };
     });
 };
 
+// Function to get the song requested by the user
 function getSong() {
+    // Variable to store the song name
     var songInput = "";
+    // If there the user did not specify a song, then the default is The Sign
     if (!process.argv[3]) {
         songInput = "The Sign Ace of Base";
     } else {
+        // Creating the song name so that are all arguments are included if the song is more than one word
         for (var i = 3; i < nodeArgs.length; i++) {
             songInput = songInput + " " + nodeArgs[i];
         }
     };
+    // Searching spotify with the song name
     spotify.search({
         type: 'track',
         query: songInput
     }, function (err, data) {
+        // If there is an error then display it on the console
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        // Displaying the different aspects of the song
         console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
         console.log("\nSong Name: " + data.tracks.items[0].name);
         console.log("\nSong Link: " + data.tracks.items[0].preview_url);
@@ -56,20 +71,23 @@ function getSong() {
     });
 };
 
+// Function to get movie requested by user
 function getMovie() {
+    // Variable to store the movie name
     var movieInput = "";
+    // Default is Mr. Nobody
     if (!process.argv[3]) {
         movieInput = "Mr.+Nobody";
     } else {
+        // Looping through to make sure we get every word in the movie name
         for (var i = 3; i < nodeArgs.length; i++) {
             movieInput = movieInput + "+" + nodeArgs[i];
         }
     };
-    
+    // Creating the URL so that we can get the API information
     var queryUrl = "http://www.omdbapi.com/?t=" + movieInput + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function (error, response, body) {
-
-                // If the request is successful
+                // If the request is successful display this information
                 if (!error && response.statusCode === 200) {
                     console.log("Movie Name: " + JSON.parse(body).Title);
                     console.log("Release Year: " + JSON.parse(body).Year);
@@ -83,17 +101,20 @@ function getMovie() {
     });
 };
 
+// Function for do-what-it-says command
 function whatItSays() {
+    // Using fs package to read the random.txt file
     fs.readFile("random.txt", "utf8", function(error, data) {
 
-        // If the code experiences any errors it will log the error to the console.
+        // If there any errors it will log the error to the console
         if (error) {
           return console.log(error);
         }
-        
+        // Splitting the text into an array so that the song is by itself
         var randomArray = data.split(",");
+        // Storing the song name in a variable
         var songInput = randomArray[1];
-
+        // Searching spotify with the song name from random.txt and displaying information
         spotify.search({
             type: 'track',
             query: songInput
